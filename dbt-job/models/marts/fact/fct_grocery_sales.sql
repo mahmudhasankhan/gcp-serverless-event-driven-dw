61f8b8a743565ccb4cea47a939a983636d23e611
mahmudhasankhan <khanzadeh008@gmail.com>
2026-05-17 23:43:05 +0600

    fixed syntax errors

--- dbt-job/models/marts/fact/fct_grocery_sales.sql
+++ dbt-job/models/marts/fact/fct_grocery_sales.sql
@@ -1,11 +1,10 @@
-{ {{
+{{
     config(
         materialized='incremental',
         unique_key='sale_key',
         on_schema_change='sync_all_columns'
     )
-}} }
-
+}}
 
 WITH sales AS (
     SELECT * FROM {{ ref('stg_sales') }}
@@ -23,17 +22,18 @@ final AS (
 
    SELECT 
         -- Surrogate Key 
-        {{ dbt_utils.generate_surrogate_key(['order_id', 'product_name']) }} AS sale_key,
+        {{ dbt_utils.generate_surrogate_key(['s.order_id', 's.product_name']) }} AS sale_key,
 
         -- Natural Key
         s.order_id,
-        -- Foreign keys to dimensions
-        {{ dbt_utils.generate_surrogate_key(['customer_id']) }} AS customer_key,
-        {{ dbt_utils.generate_surrogate_key(['product_name']) }} AS product_key,
-        {{ dbt_utils.generate_surrogate_key(['sales_person']) }} AS salesperson_key,
-        {{ dbt_utils.generate_surrogate_key(['shipper_name']) }} AS shipper_key,
-        {{ dbt_utils.generate_surrogate_key(['city', 'state', 'country', 'region']) }} AS region_key,
-        {{ dbt_utils.generate_surrogate_key(['payment_type']) }} AS payment_type_key,
+        
+        -- Foreign keys to dimensions (Added 's.' aliases here)
+        {{ dbt_utils.generate_surrogate_key(['s.customer_id']) }} AS customer_key,
+        {{ dbt_utils.generate_surrogate_key(['s.product_name']) }} AS product_key,
+        {{ dbt_utils.generate_surrogate_key(['s.sales_person']) }} AS salesperson_key,
+        {{ dbt_utils.generate_surrogate_key(['s.shipper_name']) }} AS shipper_key,
+        {{ dbt_utils.generate_surrogate_key(['s.city', 's.state', 's.country', 's.region']) }} AS region_key,
+        {{ dbt_utils.generate_surrogate_key(['s.payment_type']) }} AS payment_type_key,
 
         -- Date foreign keys
         d.date_key AS order_date_key,
